75823f5c2dc13908a3d95f3129c9517e85d03495
DynamiteAthe <DynamiteAthe@gmail.com>
2011-06-12 11:47:33 +0000

    Removed vacp project.

--- vacp/vacp_domain/src/asset.cpp
+++
@@ -1,9 +0,0 @@
-#include <vacp_domain/include/asset.h>
-#include <vacp_data/include/asset_data.h>
-
-Asset::E_ERROR Asset::load_dep_branch(void)
-{
-	void *data = AssetDAL::getDependency(this->name);
-	
-	return E_OK;
-}
