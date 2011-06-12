a3a5652aceda612043fabf610b36efc48c364306
DynamiteAthe <DynamiteAthe@gmail.com>
2011-06-12 13:16:38 +0000

    Fixed indention and project settings.

--- vacp/build.cpp
+++ vacp/build.cpp
@@ -1,56 +1,55 @@
 #include "build.h"
 #include <asset.h>
 #include <error.h>
-#include <strstream>
 #include <QFileInfo>
 
 BuildContext::BuildContext(QString &asset_name, build_callback callback)
-        :       asset(asset_name),
-                cbk(callback)
+    :       asset(asset_name),
+      cbk(callback)
 {
 
 }
 
 void BuildContext::init_dep_tree(unsigned int depth, Asset *root_asset)
 {
-        if(this->tree_depth < depth)
-                this->tree_depth = depth;
+    if(this->tree_depth < depth)
+        this->tree_depth = depth;
 
-        this->dep_map.insert(depth, root_asset);
-        std::vector<Asset*> *deps = root_asset->get_dependencies();
+    this->dep_map.insert(depth, root_asset);
+    std::vector<Asset*> *deps = root_asset->get_dependencies();
 
-        std::vector<Asset*>::iterator it;
-        for(it = deps->begin(); it < deps->end(); it++)
-        {
-                this->init_dep_tree(depth + 1, (*it));
-        }
+    std::vector<Asset*>::iterator it;
+    for(it = deps->begin(); it < deps->end(); it++)
+    {
+        this->init_dep_tree(depth + 1, (*it));
+    }
 }
 
 void BuildContext::exec(void)
 {
-        Asset *asset = Asset::get_by_name(this->asset);
-        this->tree_depth = 0;
-        this->init_dep_tree(0, asset);
-        this->build_tree();
+    Asset *asset = Asset::get_by_name(this->asset);
+    this->tree_depth = 0;
+    this->init_dep_tree(0, asset);
+    this->build_tree();
 }
 
 void BuildContext::build_tree(void)
 {
-        for(unsigned int i = this->tree_depth; i >= 0; i--)
+    for(unsigned int i = this->tree_depth; i >= 0; i--)
+    {
+        QList<Asset*> values = this->dep_map.values(i);
+        for(int j = 0; j < values.size(); j++)
         {
-            QList<Asset*> values = this->dep_map.values(i);
-            for(int j = 0; j < values.size(); j++)
-            {
-                this->build_asset(values.at(j));
-            }
+            this->build_asset(values.at(j));
         }
+    }
 }
 
 void BuildContext::build_asset(Asset* asset)
 {
     // Build asset if no output file exists
     bool should_build = asset->get_output().isEmpty() ||
-                        !QFile::exists(asset->get_output());
+            !QFile::exists(asset->get_output());
 
     if(!should_build)
     {
