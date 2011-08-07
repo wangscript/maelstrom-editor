dd192516c8fbb64459697403e4a080db5268ba55
DynamiteAthe <DynamiteAthe@gmail.com>
2011-08-07 01:36:58 +0000

    Implemented full mipmap-chain generation in DDS compiler (finally!). Only works for textures with power-of-two dimensions though.

--- content_exporters/texture/default_texture_compilers/texture_compiler.cpp
+++ content_exporters/texture/default_texture_compilers/texture_compiler.cpp
@@ -55,7 +55,26 @@ extern "C"
 		{
 			try
 			{
-				header.dwMipMapCount = ContentCompiler::str_to_int(*mipmap_value);
+				I32 mipmap = ContentCompiler::str_to_int(*mipmap_value);
+				if(mipmap)
+				{
+					if(this->is_power_of_two(header.dwHeight) && this->is_power_of_two(header.dwWidth))
+					{
+						U32 width_bit = this->get_set_bit(header.dwWidth);
+						U32 height_bit = this->get_set_bit(header.dwHeight);
+						U32 mipmap_levels = std::min(width_bit, height_bit);
+
+						header.dwMipMapCount = mipmap_levels;
+					}
+					else
+					{
+						// handle non-power-of-two textures.
+					}
+				}
+				else
+				{
+					header.dwMipMapCount = 1;
+				}
 				//header.dwHeaderFlags |= DDS_HEADER_FLAGS_MIPMAP;
 			}
 			catch(std::exception &ex)
@@ -117,30 +136,43 @@ extern "C"
 		return 0;
 	}
 
+	bool TextureDDSCompiler::is_power_of_two(U32 value)
+	{
+		U32 max = (1 << 31);
+		U32 v = 1;
+		U32 bits = 0;
+
+		// loop and a half...
+		if(value & v)
+			bits++;
+		do
+		{
+			v <<= 1;
+			if(value & v)
+				bits++;
+		}while(v != max);
+
+		return bits == 1;
+	}
+
+	U32 TextureDDSCompiler::get_set_bit(U32 value)
+	{
+		U32 max = (1 << 31);
+		U32 v = 1;
+		U32 bits = 0;
+		bool bit_found;
+		do
+		{
+			bits++;
+			bit_found = (value & v) != 0;
+			v <<= 1;
+		}while(!bit_found && v != max);
+
+		return bits;
+	}
+
 	void TextureDDSCompiler::serialize_dds_header(DDS_HEADER& header, std::ofstream& output)
 	{
-		/*output << DDS_MAGIC;
-		output << header.dwSize;
-		output << header.dwHeaderFlags;
-		output << header.dwHeight;
-		output << header.dwWidth;
-		output << header.dwPitchOrLinearSize;
-		output << header.dwDepth;
-		output << header.dwMipMapCount;
-		for(int i = 0; i < 11; i++)
-			output << header.dwReserved1[i];
-		output << header.ddspf.dwSize;
-		output << header.ddspf.dwFlags;
-		output << header.ddspf.dwFourCC;
-		output << header.ddspf.dwRGBBitCount;
-		output << header.ddspf.dwRBitMask;
-		output << header.ddspf.dwGBitMask;
-		output << header.ddspf.dwBBitMask;
-		output << header.ddspf.dwABitMask;
-		output << header.dwSurfaceFlags; //dwCaps on MSDN documentation
-		output << header.dwCubemapFlags; // dwCaps2 on MSDN documentation;
-		for(int i = 0; i < 3; i++)
-			output << header.dwReserved2[i];*/
 		I32 magic = DDS_MAGIC;
 		int beforep = output.tellp();
 		output.write(reinterpret_cast<char*>(&magic), 4);
@@ -194,11 +226,89 @@ extern "C"
 			return -1;
 		}
 
-		output.write(data, *data_len);
+		mipmap_chain *mm_chain = generate_mipmap_chain(data, *data_len, header.dwHeight, header.dwWidth, header.dwMipMapCount);
+		for(int l = 0; l <= header.dwMipMapCount; l++)
+		{
+			output.write(mm_chain->chain[l].data, mm_chain->chain[l].length);
+		}
+		destroy_mipmap_chain(mm_chain);
 
 		output.close();
 
 		return 0;
+	}
+
+	mipmap_chain *TextureDDSCompiler::generate_mipmap_chain(char *data, U32 data_length, U32 height, U32 width, U32 chain_length)
+	{
+		mipmap_chain *mm_chain = new mipmap_chain;
+		mm_chain->length = chain_length;
+		mm_chain->chain = new texture2d[chain_length];
+
+		mm_chain->chain[0].data = new char[data_length];
+		mm_chain->chain[0].length = data_length;
+		memcpy(mm_chain->chain[0].data, data, data_length);
+
+		U32 mipmap_width = width;
+		U32 mipmap_height = height;
+
+		U32 cur_width;
+		U32 pre_width = width;
+
+		U32 *cur_level;
+		U32 *pre_level;
+
+		U32 box[4];
+
+		for(int l = 1; l <= chain_length; l++)
+		{
+			mipmap_width >>= 1;
+			mipmap_height >>= 1;
+
+			cur_width = mipmap_width;
+			mm_chain->chain[l].length = mipmap_width * mipmap_height * 4;
+			mm_chain->chain[l].data = new char[mm_chain->chain[l].length];
+			cur_level = reinterpret_cast<U32*>(mm_chain->chain[l].data);
+			pre_level = reinterpret_cast<U32*>(mm_chain->chain[l-1].data);
+
+			for(int y = 0; y < std::max((U32)1, mipmap_height); y++)
+			{
+				float y0 = y << 1;
+				float y1 = std::min(y0 + 1, (float)std::max((U32)1, height >> (l-1)) - 1);
+				for(int x = 0; x < std::max((U32)1, mipmap_width); x++)
+				{
+					float x0 = x<<1;
+					float x1 = std::min(x0 + 1, (float)std::max((U32)1, width>>(l-1)) - 1);
+					
+					box[0] = pre_level[(U32)(y0 * pre_width) + (U32)x0];
+					box[1] = pre_level[(U32)(y0 * pre_width) + (U32)x1];
+					box[2] = pre_level[(U32)(y1 * pre_width) + (U32)x0];
+					box[3] = pre_level[(U32)(y1 * pre_width) + (U32)x1];
+
+					cur_level[(y * cur_width) + x] =	((U32)(((box[0] >> 24) + (box[1] >> 24) + (box[2] >> 24) + (box[3] >> 24)) * 0.25) << 24) |
+														((U32)(((box[0] >> 16 & 0xFF) + (box[1] >> 16 & 0xFF) + (box[2] >> 16 & 0xFF) + (box[3] >> 16 & 0xFF)) * 0.25) << 16) |
+														((U32)(((box[0] >> 8 & 0xFF) + (box[1] >> 8 & 0xFF) + (box[2] >> 8 & 0xFF) + (box[3] >> 8 & 0xFF)) * 0.25) << 8) |
+														(U32)(((box[0] & 0xFF) + (box[1] & 0xFF) + (box[2] & 0xFF) + (box[3] & 0xFF)) * 0.25);
+				}
+			}
+
+			pre_width = cur_width;
+		}
+
+
+		return mm_chain;
+	}
+
+	void TextureDDSCompiler::destroy_mipmap_chain(mipmap_chain *mm_chain)	
+	{
+		for(U32 i = 0; i < mm_chain->length; i++)
+		{
+			delete[] mm_chain->chain[i].data;
+		}
+
+		// Cant do this.. why?
+		//delete[] mm_chain->chain;
+
+		delete mm_chain;
 	}
 
 	class TextureDDSCompilerFactory : public ContentCompilerFactory
