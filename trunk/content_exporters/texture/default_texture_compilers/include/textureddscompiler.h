77a8e80e108398d2d7d6977b53d07b2181e0fe3e
DynamiteAthe@gmail.com <DynamiteAthe@gmail.com@6e2f20b4-5912-c7f0-9b85-069617bd5e4c>
2011-08-07 01:36:58 +0000

    Implemented full mipmap-chain generation in DDS compiler (finally!). Only works for textures with power-of-two dimensions though.

--- trunk/content_exporters/texture/default_texture_compilers/include/textureddscompiler.h
+++ trunk/content_exporters/texture/default_texture_compilers/include/textureddscompiler.h
@@ -4,11 +4,27 @@
 #include <../../../../vacp/plugin_header/vacp_plugin_common.h>
 #include <ddsdef.h>
 
+struct texture2d
+{
+	char *data;
+	U32 length;
+};
+
+struct mipmap_chain
+{
+	texture2d *chain;
+	U32 length;
+};
+
 class TextureDDSCompiler : public ContentCompiler
 	{
 	private:
 		int prepare_dds_header(DDS_HEADER &header, IContent *input);
 		void serialize_dds_header(DDS_HEADER& header, std::ofstream& output);
+		bool is_power_of_two(U32 value);
+		U32 get_set_bit(U32 value);
+		mipmap_chain *generate_mipmap_chain(char *data, U32 data_length, U32 height, U32 width, U32 chain_length);
+		void destroy_mipmap_chain(mipmap_chain *mm_chain);
 	public:
 		virtual int process(IContent *input, const char *path, char *config);
 	};
